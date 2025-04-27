89006316066af93ec9804192950351e619f1692a
Zhero-Six <vmochoge@gmail.com>
2025-04-27 17:41:13 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+      <h1>Hi, I'm (your name)</h1>
+      <img alt="My profile pic" src="https://via.placeholder.com/350" />
+      <h2>About Me</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
